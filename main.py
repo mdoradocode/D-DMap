97ccb0d0e91f13db0d2eb6d3065540ab2137cca5
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2020-11-09 21:40:58 -0800

    Basic program I/O work, Off button working
    This allows the user to click a specfic part of the screen and close the window out, an exit button if I have ever seen one

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import sys
-from graphics import GraphWin
+import time
+from graphics import *
 
 def main(): 
-   win = GraphWin("This is a window", 800, 800, True)
-   print(win.getMouse())
-   #win.close() 
+   
+   win = GraphWin("This is a window", 500, 500, True)
+   R = Rectangle(Point(50,50),Point(100,100))
+   R.draw(win)
+   global closeX, closeY, mousePoint
+   mousePoint = win.getMouse()
+   closeY = mousePoint.getY()
+   closeX = mousePoint.getX()
+   while (((closeX >= 50) and (closeY >= 50)) and ((closeX<=  100) and (closeY <= 100))) != True:
+      start = time.time()
+      mousePoint = win.getMouse()
+      print(mousePoint)
+      closeY = mousePoint.getY()
+      closeX = mousePoint.getX()
+      print(time.time() - start)
+   win.close()
 
 main()
